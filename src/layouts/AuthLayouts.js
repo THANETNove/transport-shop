73e9548d951a01823c4fc44b7941c539f3ecc125
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-08-12 23:51:41 +0700

    ใช้  css 1 อัน

--- src/layouts/AuthLayouts.js
+++ src/layouts/AuthLayouts.js
@@ -29,29 +29,6 @@ const Auth = (props) => {
 
   return (
     <>
-      <Helmet>
-        <link
-          href="assetsAuth/vendor/fontawesome-free/css/all.min.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-          rel="stylesheet"
-        />
-        <link href="assetsAuth/css/sb-admin-2.min.css" rel="stylesheet" />
-
-        {/* body */}
-
-        <script src="assetsAuth/vendor/jquery/jquery.min.js"></script>
-        <script src="assetsAuth/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-        <script src="assetsAuth/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-        <script src="assetsAuth/js/sb-admin-2.min.js"></script>
-
-        <script src="assetsAuth/vendor/chart.js/Chart.min.js"></script>
-      </Helmet>
       <div id="wrapper">
         <Sidebar />
         <div id="content-wrapper" class="d-flex flex-column">
