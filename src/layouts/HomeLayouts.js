a252bea6b627337b41fe99e6cc8af5f27de03772
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-08-12 17:09:07 +0700

    ใส่ css หน้า home

--- src/layouts/HomeLayouts.js
+++ src/layouts/HomeLayouts.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useLocation, Route, Routes, Navigate } from "react-router-dom";
+import { Helmet } from "react-helmet";
 
 import routes from "../routes.js";
 
@@ -20,6 +21,54 @@ const Home = (props) => {
 
   return (
     <>
+      <Helmet>
+        <link
+          href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Nunito:300,300i,400,400i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i"
+          rel="stylesheet"
+        />
+
+        <link href="/assets/vendor/aos/aos.css" rel="stylesheet" />
+        <link
+          href="/assets/vendor/bootstrap/css/bootstrap.min.css"
+          rel="stylesheet"
+        />
+        <link
+          href="/assets/vendor/bootstrap-icons/bootstrap-icons.css"
+          rel="stylesheet"
+        />
+        <link
+          href="/assets/vendor/glightbox/css/glightbox.min.css"
+          rel="stylesheet"
+        />
+        <link href="/assets/vendor/remixicon/remixicon.css" rel="stylesheet" />
+        <link
+          href="/assets/vendor/swiper/swiper-bundle.min.css"
+          rel="stylesheet"
+        />
+        <link href="/assets/css/style.css" rel="stylesheet" />
+
+        <script
+          async
+          src="/assets/vendor/purecounter/purecounter_vanilla.js"
+        ></script>
+        <script async src="/assets/vendor/aos/aos.js"></script>
+        <script
+          async
+          src="/assets/vendor/bootstrap/js/bootstrap.bundle.min.js"
+        ></script>
+        <script
+          async
+          src="/assets/vendor/glightbox/js/glightbox.min.js"
+        ></script>
+        <script
+          async
+          src="/assets/vendor/isotope-layout/isotope.pkgd.min.js"
+        ></script>
+        <script async src="/assets/vendor/swiper/swiper-bundle.min.js"></script>
+        <script async src="/assets/vendor/php-email-form/validate.js"></script>
+
+        <script async src="/assets/js/main.js"></script>
+      </Helmet>
       <Routes>
         {getRoutes(routes)}
         <Route path="*" element={<Navigate to="/" replace />} />
