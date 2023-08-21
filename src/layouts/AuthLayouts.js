64db9d7697c9a616c797d69bcb1ce89aae1fe0a5
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-08-21 09:37:00 +0700

    className

--- src/layouts/AuthLayouts.js
+++ src/layouts/AuthLayouts.js
@@ -84,7 +84,7 @@ const Auth = (props) => {
       ) : (
         <div id="wrapper">
           <Sidebar />
-          <div id="content-wrapper" class="d-flex flex-column">
+          <div id="content-wrapper" className="d-flex flex-column">
             <div id="content">
               <Navbar />
               <Routes>
