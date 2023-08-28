5bdb34f6ff08a1be7d480b0e7189da20e70782c9
thanet@planforfit.com <lollipopwar-love@hotmail.co.th>
2023-08-29 00:29:31 +0700

    เเก้ error

--- src/layouts/AuthLayouts.js
+++ src/layouts/AuthLayouts.js
@@ -50,16 +50,7 @@ const Auth = (props) => {
   return (
     <>
       <Helmet>
-        <link
-          href="assetsAuth/vendor/fontawesome-free/css/all.min.css"
-          rel="stylesheet"
-          type="text/css"
-        />
-        <link
-          href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-          rel="stylesheet"
-        />
-        <link href="assetsAuth/css/sb-admin-2.css" rel="stylesheet" />
+       
 
         {(location.pathname === "/login" ||
           location.pathname === "/register") && (
@@ -79,17 +70,9 @@ const Auth = (props) => {
           </style>
         )}
 
-        {/* body */}
 
-        <script src="assetsAuth/vendor/jquery/jquery.min.js"></script>
-        <script src="assetsAuth/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
 
-        <script src="assetsAuth/vendor/jquery-easing/jquery.easing.min.js"></script>
-
-        <script src="assetsAuth/js/sb-admin-2.min.js"></script>
-
-        <script src="assetsAuth/vendor/chart.js/Chart.min.js"></script>
-      </Helmet>
+      </Helmet> 
 
       {location.pathname == "/login" || location.pathname == "/register" ? (
         <>
